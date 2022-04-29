9cc18311c604158bd386ae700db5c23bec31a33d
Jennifer Glauche <mail@jglauche.de>
2022-04-29 21:02:36 +0200

    make hull example different from slot functionality, add color

--- output/hull_example.scad
+++ output/hull_example.scad
@@ -1,8 +1,8 @@
 module HullExample(){
-  translate([2.5, 2.5, 0])hull(){
-      cylinder(d=5,h=10,$fn=64);
-      translate([10, 0, 0])cylinder(d=5,h=10,$fn=64);
-    }
+  translate([5, 5, 0])color("SteelBlue")hull(){
+        cylinder(d=10,h=10,$fn=64);
+        translate([10, 10, 0])cylinder(d=5,h=10,$fn=64);
+      }
 }
 $fn=64;
-color("SteelBlue")HullExample();
+HullExample();
--- parts/hull_example.rb
+++ parts/hull_example.rb
@@ -1,15 +1,17 @@
 class HullExample < Part
   def initialize(opts={})
-    @d = 5
+    @d = 10
+    @d2 = 5
     @h = 10
-
   end
 
   def part
+    # This is basically how the OpenSCAD hull works.
     res = hull(
       cylinder(d: @d, h: @h),
-      cylinder(d: @d, h: @h).move(x: 10)
+      cylinder(d: @d2, h: @h).move(xy: 10)
     ).moveh(xy: @d)
-    res
+
+    res.color("SteelBlue")
   end
 end
